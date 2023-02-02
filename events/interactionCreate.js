6d86c193a0a0e5847d173efba7116ed2297908d2
DinoDev <brankovicdino851@gmail.com>
2023-02-02 15:26:24 +0100

    Added interactions

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,23 +1,28 @@
+/* eslint-disable no-empty */
 const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isChatInputCommand()) {
 
-		const command = interaction.client.commands.get(interaction.commandName);
-
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
 		}
+		else if (interaction.isAutocomplete) {
 
-		try {
-			await command.execute(interaction);
-		}
-		catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+
+			try {
+				await command.autocomplete(interaction);
+			}
+			catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
 		}
 	},
 };
