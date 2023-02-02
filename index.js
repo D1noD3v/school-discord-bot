c32e0fd9d7e0e37b5be34d33984d375a7a8ddaeb
DinoDev <brankovicdino851@gmail.com>
2023-02-02 15:24:57 +0100

    replaced uneccecary code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -28,25 +28,5 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
-
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		// await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
 
 client.login(token);
